dbdb756d2fdb942b1855f23788b4f235c3be144d
IhsanHalimun <54117660+IhsanHalimun@users.noreply.github.com>
2020-03-12 18:49:39 -0700

    Updated Directory, FileSystem, FileTable, & FileTableEntry

--- Changed Files/Directory.java
+++ Changed Files/Directory.java
@@ -1,63 +1,100 @@
 public class Directory {
-   private static int maxChars = 30; // max characters of each file name
+    private static int maxChars = 30; // max characters of each file name
 
-   // Directory entries
-   private int fsize[];        // each element stores a different file size.
-   private char fnames[][];    // each element stores a different file name.
+    // Directory entries
+    private int fsize[]; // each element stores a different file size.
+    private char fnames[][]; // each element stores a different file name.
 
-   public Directory( int maxInumber ) { // directory constructor
-      fsize = new int[maxInumber];     // maxInumber = max files
-      for ( int i = 0; i < maxInumber; i++ ) 
-         fsize[i] = 0;                 // all file size initialized to 0
-      fnames = new char[maxInumber][maxChars];
-      String root = "/";                // entry(inode) 0 is "/"
-      fsize[0] = root.length( );        // fsize[0] is the size of "/".
-      root.getChars( 0, fsize[0], fnames[0], 0 ); // fnames[0] includes "/"
-   }
+    public Directory(int maxInumber) { // directory constructor
+        fsize = new int[maxInumber]; // maxInumber = max files
+        for (int i = 0; i < maxInumber; i++)
+            fsize[i] = 0; // all file size initialized to 0
+        fnames = new char[maxInumber][maxChars];
+        String root = "/"; // entry(inode) 0 is "/"
+        fsize[0] = root.length(); // fsize[0] is the size of "/".
+        root.getChars(0, fsize[0], fnames[0], 0); // fnames[0] includes "/"
+    }
 
-   public void bytes2directory( byte data[] ) {
-      // assumes data[] received directory information from disk
-      // initializes the Directory instance with this data[]
+    public void bytes2directory(byte data[]) {
+        // assumes data[] received directory information from disk
+        // initializes the Directory instance with this data[]
 
-      int seek = 0;
+        int seek = 0;
 
-      for(int i = 0; i < this.fsize.length; i++)
-      {
-         this.fsize[i] = SysLib.bytes2int(data, seek);
+        for (int i = 0; i < this.fsize.length; i++) {
+            this.fsize[i] = SysLib.bytes2int(data, seek);
 
-         seek += (Integer.SIZE / Byte.SIZE);
-      }
+            seek += (Integer.SIZE / Byte.SIZE);
+        }
 
-      int maxLength = maxChars * Character.SIZE / Byte.SIZE;
+        int maxLength = maxChars * Character.SIZE / Byte.SIZE;
 
 
-      for(int i = 0; i < this.fnames.length; i++)
-      {
-         String fileName = new String(data, seek, maxLength);
-         fileName.getChars(0, this.fsize[i], this.fnames[i], 0);
+        for (int i = 0; i < this.fnames.length; i++) {
+            String fileName = new String(data, seek, maxLength);
+            fileName.getChars(0, this.fsize[i], this.fnames[i], 0);
 
-         seek += maxLength;
-      }
-   }
+            seek += maxLength;
+        }
+    }
 
-   public byte[] directory2bytes( ) {
-      // converts and return Directory information into a plain byte array
-      // this byte array will be written back to disk
-      // note: only meaningfull directory information should be converted
-      // into bytes.
-   }
+    public byte[] directory2bytes() {
+        // converts and return Directory information into a plain byte array
+        // this byte array will be written back to disk
+        // note: only meaningfull directory information should be converted
+        // into bytes.
 
-   public short ialloc( String filename ) {
-      // filename is the one of a file to be created.
-      // allocates a new inode number for this filename
-   }
+        byte[] buffer = new byte[this.fsize.length * (Integer.SIZE / Byte.SIZE) + this.fnames.length * (Character.SIZE / Byte.SIZE) * maxChars];
 
-   public boolean ifree( short iNumber ) {
-      // deallocates this inumber (inode number)
-      // the corresponding file will be deleted.
-   }
+        int seek = 0;
 
-   public short namei( String filename ) {
-      // returns the inumber corresponding to this filename
-   }
-}
+        for (int i = 0; i < this.fsize.length; i++) {
+            SysLib.int2bytes(this.fsize[i], buffer, seek);
+            seek += (Integer.SIZE / Byte.SIZE);
+        }
+
+        int maxLength = maxChars * Character.SIZE / Byte.SIZE;
+
+
+        for (int i = 0; i < this.fnames.length; i++) {
+            String fileName = new String(this.fname[i], 0, this.fsize[i]);
+            byte[] fNameBytes = fileName.getBytes();
+
+            System.arraycopy(fileName, 0, buffer, seek, fNameBytes.length);
+
+            seek += maxLength;
+        }
+    }
+
+    public short ialloc(String fileName) {
+        // filename is the one of a file to be created.
+        // allocates a new inode number for this filename
+        for (int i = 1; i < this.fsize.length(); ++i) {
+            if (this.fsize[i] == 0) {
+                this.fsize[i] = fileName.length() < maxChars ? fileName.length : maxChars;
+                fileName.getChars(0, this.fsize[i], this.fnames[i], 0);
+                return (short) i;
+            }
+            return -1;
+        }
+
+        public boolean ifree(short iNumber) {
+            // deallocates this inumber (inode number)
+            // the corresponding file will be deleted.
+            if (this.fsize[iNumber] <= 0) {
+                return false;
+            }
+            this.fsize[iNumber] = 0;
+            return true;
+        }
+
+        public short namei(String fileName) {
+            // returns the inumber corresponding to this filename
+            for (int i = 0; int i < this.fsize.length, i++) {
+                String fileNameCheck = new String(this.fname[i], 0, this.fsize[i]);
+                if (fileName.equals(fileNameCheck))
+                    return (short) i;
+            }
+            return -1;
+        }
+    }
--- Changed Files/FileTable.java
+++ Changed Files/FileTable.java
@@ -1,31 +1,103 @@
-
 public class FileTable {
 
-   private Vector table;         // the actual entity of this file table
-   private Directory dir;        // the root directory 
+    private Vector table; // the actual entity of this file table
+    private Directory dir; // the root directory 
 
-   public FileTable( Directory directory ) { // constructor
-      table = new Vector( );     // instantiate a file (structure) table
-      dir = directory;           // receive a reference to the Director
-   }                             // from the file system
+    public FileTable(Directory directory) { // constructor
+        table = new Vector(); // instantiate a file (structure) table
+        dir = directory; // receive a reference to the Director
+    } // from the file system
 
-   // major public methods
-   public synchronized FileTableEntry falloc( String filename, String mode ) {
-      // allocate a new file (structure) table entry for this file name
-      // allocate/retrieve and register the corresponding inode using dir
-      // increment this inode's count
-      // immediately write back this inode to the disk
-      // return a reference to this file (structure) table entry
-   }
+    // major public methods
+    public synchronized FileTableEntry falloc(String fileName, String mode) {
+        // allocate a new file (structure) table entry for this file name
+        // allocate/retrieve and register the corresponding inode using dir
+        // increment this inode's count
+        // immediately write back this inode to the disk
+        // return a reference to this file (structure) table entry
 
-   public synchronized boolean ffree( FileTableEntry e ) {
-      // receive a file table entry reference
-      // save the corresponding inode to the disk
-      // free this file table entry.
-      // return true if this file table entry found in my table
-   }
+        Inode newInode = null;
 
-   public synchronized boolean fempty( ) {
-      return table.isEmpty( );  // return if table is empty 
-   }                            // should be called before starting a format
+        short iNumber = 0;
+
+        if (fileName.equals("/")) { // root
+            iNumber = 0;
+        } else {
+            iNumber = this.dir.namei(fileName); // get iNumber from directory
+        }
+
+        while (true) {
+            if (iNumber >= 0) {
+                newInode = new Inode(iNumber);
+                if (mode.equals("r")) {
+                    if (newInode.flag == 0)
+                        newInode.flag = 1; // if unused, change flag
+
+                    else if (newInode.flag != 1) { // if not being read or written to?
+                        try {
+                            this.wait();
+                        } catch (InterruptedException var7) {;
+                        }
+                        continue;
+                    }
+                    break;
+                }
+
+                if (newInode.flag != 0 && newInode.flag != 3) {
+                    if (newInode.flag == 1 || newInode.flag == 2) {
+                        newInode.flag = (short)(newInode.flag + 3);
+                        newInode.toDisk(iNumber);
+                    }
+
+                    try {
+                        this.wait();
+                    } catch (InterruptedException var6) {;
+                    }
+                    continue;
+                }
+
+                newInode.flag = 2;
+                break;
+            }
+
+            if (mode.equals("r")) {
+                return null;
+            }
+
+            iNumber = this.dir.ialloc(fileName);
+            newInode = new Inode();
+            newInode.flag = 2;
+            break;
+        }
+
+        newInode.count++;
+
+        newInode.toDisk(iNumber);
+
+        FileTableEntry newEntry = new FileTableEntry(newInode, iNumber, mode);
+        this.table.addElement(newEntry);
+
+        return newEntry;
+    }
+
+    public synchronized boolean ffree(FileTableEntry e) {
+        // receive a file table entry reference
+        // save the corresponding inode to the disk
+        // free this file table entry.
+        // return true if this file table entry found in my table
+
+        if (this.table.removeElement(e)) {
+            e.inode.count--; //remove inode
+            e.inode.flag = 0;
+            e.inode.toDisk(e.iNumberber);
+            e = null;
+            this.notify();
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized boolean fempty() {
+        return table.isEmpty(); // return if table is empty 
+    } // should be called before starting a format
 }
